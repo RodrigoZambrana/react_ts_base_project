47b2895fbeb8267ff1e3061fa9b9788102ff45a4
Rodrigo Zambrana <rodrigo@lacnic.net>
2023-07-17 22:42:30 -0300

    primer commit

---
+++ src/mock/data/authData.ts
@@ -0,0 +1,20 @@
+export const signInUserData = [
+    {
+        id: '21',
+        avatar: '/img/avatars/thumb-1.jpg',
+        userName: 'Carolyn Perkins',
+        email: 'carolyn.p@elstar.com',
+        authority: ['admin', 'user'],
+        password: '123Qwe',
+        accountUserName: 'admin',
+    },
+    {
+        id: '22',
+        avatar: '/img/avatars/thumb-1.jpg',
+        userName: 'Rodrigo Zambrana',
+        email: 'carolyn.p@elstar.com',
+        authority: ['admin', 'user'],
+        password: '123Qwe',
+        accountUserName: 'Rodrigo',
+    },
+]
